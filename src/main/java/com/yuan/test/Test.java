9b41138b2f85c641c73dd4131f1c526b7e425b5a
lx <lx@163.com>
2022-06-05 00:00:05 +0800

    push commit

--- src/main/java/com/yuan/test/Test.java
+++ src/main/java/com/yuan/test/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("hello4");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
